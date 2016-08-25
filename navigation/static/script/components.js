d81fbe9a6978f660be51ef4c40f82b70d0e48e9c
hinikai <hinikai@gmail.com>
2016-08-25 12:12:31 +0800

    update

--- navigation/static/script/components.js
+++ navigation/static/script/components.js
@@ -1,36 +1,39 @@
 
 var Panel = Vue.extend({
-    template: '#luwang-form-template',
+    template: '#form-template',
     data: function () {
         return {   
             mode: 'driving',
             tactics: 12,
             starts: [{
-                name: ''
+                name: '颐和园'
             }],
             ends: [{
-                name: ''
-            }]
+                name: '西单'
+            }],
+            routes: []
         }
     },
     methods: {
         search: function () {
             for (var i = 0; i < this.starts.length; i++) {
                 for (var j = 0; j < this.ends.length; j++) {
-                    var data = {
-                        mode: this.mode,
-                        tactics: this.tactics,
-                        origin: this.starts[i].name,
-                        destination: this.ends[j].name,
-                        origin_region: map.getCurrentCity().name,
-                        destination_region: map.getCurrentCity().name,
-                        region: map.getCurrentCity().name,
-                        output: 'json',
-                        //tactics: 10,
-                        ak: 'aONgqBx7eaWfv2X6RiMyGcWxp6HAwFWi'
-                    }
+                    if (this.starts[i].name && this.ends[j].name) {
+                        var data = {
+                            mode: this.mode,
+                            tactics: this.tactics,
+                            origin: this.starts[i].name,
+                            destination: this.ends[j].name,
+                            origin_region: map.getCurrentCity().name,
+                            destination_region: map.getCurrentCity().name,
+                            region: map.getCurrentCity().name,
+                            output: 'json',
+                            //tactics: 10,
+                            ak: 'aONgqBx7eaWfv2X6RiMyGcWxp6HAwFWi'
+                        }
 
-                    this.request(data);
+                        this.request(data);
+                    }
                 }
             }
             
@@ -39,6 +42,7 @@ var Panel = Vue.extend({
             var me = this;
             var url = 'http://api.map.baidu.com/direction/v1?callback=?';
             $.getJSON(url, data, function (rs) {
+                me.routes = rs.result.routes;
                 if (me.mode == 'transit') { //公交
                     var routesLength = rs.result.routes.length;
                     for (var i = 0; i< routesLength; i++) {
@@ -51,7 +55,6 @@ var Panel = Vue.extend({
                         }
                         paths = paths.join(';');
                         paths = paths.split(';');
-                        console.log(paths);
                         for (var j = 0; j < paths.length; j++) {
                             var lnglat = paths[j].split(',');
                             points.push(new BMap.Point(lnglat[0], lnglat[1]));
@@ -99,4 +102,10 @@ var Panel = Vue.extend({
     }
 });
 
+var RouteList = Vue.extend({
+    template: '#route-list',
+    props: ['routes']
+});
+
 Vue.component('panel', Panel)
+Vue.component('route-list', RouteList )

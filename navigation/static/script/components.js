3eae5a3aa127af4dede5c3ba434603e63d0ea20d
hinikai <hinikai@gmail.com>
2016-08-28 22:34:48 +0800

    update

--- navigation/static/script/components.js
+++ navigation/static/script/components.js
@@ -248,11 +248,11 @@ var LabelList = Vue.extend({
         };
 
         var styleOptions = {
-            strokeColor:"red",    //边线颜色。
-            fillColor:"red",      //填充颜色。当参数为空时，圆形将没有填充效果。
+            strokeColor:"blue",    //边线颜色。
+            fillColor:"blue",      //填充颜色。当参数为空时，圆形将没有填充效果。
             strokeWeight: 3,       //边线的宽度，以像素为单位。
-            strokeOpacity: 0.8,       //边线透明度，取值范围0 - 1。
-            fillOpacity: 0.6,      //填充的透明度，取值范围0 - 1。
+            strokeOpacity: 0.5,       //边线透明度，取值范围0 - 1。
+            fillOpacity: 0.2,      //填充的透明度，取值范围0 - 1。
             strokeStyle: 'solid' //边线的样式，solid或dashed。
         }
 
@@ -289,6 +289,13 @@ var LabelList = Vue.extend({
             this.markers.forEach(function (item) {
                 map.removeOverlay(item);
             });
+        },
+        remove: function (index) {
+            map.removeOverlay(this.markers[index]);
+            map.removeOverlay(this.tips[index]);
+            this.markers.splice(index, 1)
+            this.tips.splice(index, 1)
+            this.labels.splice(index, 1)
         }
     }
 });

c5530e2e5800869833931fa62f07291877edef03
hinikai <hinikai@gmail.com>
2016-08-29 19:40:05 +0800

    updatge

--- navigation/static/script/components.js
+++ navigation/static/script/components.js
@@ -1,9 +1,58 @@
 function Route(options) {
     options = options || {};
+    this.color = '';
+    this.lineStyle = {
+        strokeColor: '#65b320',
+        strokeOpacity: 0.8,
+        strokeWeight: 5,
+    }
+    this.colors = [
+        '#b71c1c',
+        '#880e4f',
+        '#4a148c',
+        '#d50000',
+        '#c51162',
+        '#aa00ff',
+        '#311b92',
+        '#1a237e',
+        '#0d47a1',
+        '#6200ea',
+        '#304ffe',
+        '#2962ff',
+        '#01579b',
+        '#006064',
+        '#004d40',
+        '#0091ea',
+        '#00b8d4',
+        '#00bfa5',
+        '#1b5e20',
+        '#33691e',
+        '#827717',
+        '#00c853',
+        '#64dd17',
+        '#aeea00',
+        '#f57f17',
+        '#ff6f00',
+        '#e65100',
+        '#ffd600',
+        '#ffab00',
+        '#ff6d00',
+        '#bf360c',
+        '#3e2723',
+        '#212121',
+        '#dd2c00',
+        '#263238',
+        '#000',
+        '#fff',
+    ];
     this.mode = options.mode;
     this._show = false;
     this.result = options.result;
     this.render();
+}
+
+Route.prototype.changeColor = function (color) {
+    this.polyline.setStrokeColor(color);
 }
 
 Route.prototype.clear = function () {
@@ -46,11 +95,7 @@ Route.prototype.render = function () {
 
         this.startMarker = addStart(points[0]);
         this.endMarker = addEnd(points[points.length - 1]);
-        this.polyline = new BMap.Polyline(points, {
-            strokeColor: '#65b320',
-            strokeOpacity: 0.8,
-            strokeWeight: 5,
-        });
+        this.polyline = new BMap.Polyline(points, this.lineStyle);
         map.addOverlay(this.polyline);
     } else { // 驾车
         var route = this.result;
@@ -69,11 +114,7 @@ Route.prototype.render = function () {
        
         this.startMarker = addStart(points[0]);
         this.endMarker = addEnd(points[points.length - 1]);
-        this.polyline = new BMap.Polyline(points, {
-            strokeColor: '#65b320',
-            strokeOpacity: 0.8,
-            strokeWeight: 5,
-        });
+        this.polyline = new BMap.Polyline(points, this.lineStyle);
         map.addOverlay(this.polyline);
     }
 }
@@ -201,6 +242,10 @@ var SelectedList = Vue.extend({
     template: '#selected-list',
     props: ['routes'],
     methods: {
+        changeColor: function (index, color) {
+            console.log(index);
+            this.routes[index].changeColor(color);
+        },
         deleteOne: function (index) {
             this.routes[index].selected = false;
             this.routes[index].clear();

13ced3efe6c707e4b2349fb322d50e8d7cc26f10
hinikai <hinikai@gmail.com>
2016-08-29 17:53:47 +0800

    update

--- navigation/static/script/components.js
+++ navigation/static/script/components.js
@@ -270,8 +270,8 @@ var LabelList = Vue.extend({
         var styleOptions = {
             strokeColor:"blue",    //边线颜色。
             fillColor:"blue",      //填充颜色。当参数为空时，圆形将没有填充效果。
-            strokeWeight: 3,       //边线的宽度，以像素为单位。
-            strokeOpacity: 0.5,       //边线透明度，取值范围0 - 1。
+            strokeWeight: 5,       //边线的宽度，以像素为单位。
+            strokeOpacity: 0.8,       //边线透明度，取值范围0 - 1。
             fillOpacity: 0.2,      //填充的透明度，取值范围0 - 1。
             strokeStyle: 'solid' //边线的样式，solid或dashed。
         }

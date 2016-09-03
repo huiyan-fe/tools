7f7197caf14ae3347ea23afa6764e0367dc0773b
hinikai <hinikai@gmail.com>
2016-09-03 11:02:50 +0800

    update

--- navigation/static/script/app.js
+++ navigation/static/script/app.js
@@ -4,13 +4,19 @@ var map = new BMap.Map("map", {
 map.setDefaultCursor('default');
 map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
 map.addControl(new BMap.MapTypeControl({
-    anchor: BMAP_ANCHOR_TOP_LEFT
+    anchor: BMAP_ANCHOR_TOP_LEFT,
+    offset: new BMap.Size(450, 10),
 }));   //添加地图类型控件
 map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
 map.panBy(180, 0);
 
 var style = 'grayscale';
 map.setMapStyle({style:style});
+
+map.addControl(new BMap.CityListControl({
+    anchor: BMAP_ANCHOR_TOP_LEFT,
+    offset: new BMap.Size(380, 10),
+}));
 
 
 var local = new BMap.LocalSearch(map, {

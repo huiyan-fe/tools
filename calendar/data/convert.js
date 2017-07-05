8d1f611813d22f08065673dcd4384ce18968a756
zmofei <13761509829@163.com>
2017-07-05 14:06:32 +0800

    update

--- calendar/data/convert.js
+++ calendar/data/convert.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-let filename = [__dirname + '/201701', __dirname + '/201702', __dirname + '/201703', __dirname + '/201704', __dirname + '/201705'];
+let filename = [__dirname + '/201701', __dirname + '/201702', __dirname + '/201703', __dirname + '/201704', __dirname + '/201705', __dirname + '/201706'];
 let weathers = {}
 filename.forEach(file => {
     fs.readFile(file, (err, data) => {

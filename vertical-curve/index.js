913c7a969886d9788e5b7353f2824ff76a51b97a
zmofei <13761509829@163.com>
2017-07-11 11:37:04 +0800

    update

--- vertical-curve/index.js
+++ vertical-curve/index.js
@@ -5,7 +5,7 @@ class Chart {
         let paddig = this.padding = [20, 20, 80, 20];
         let canvas = document.createElement('canvas');
         let ctx = this.ctx = canvas.getContext('2d');
-        this.data = data.filter((item, index) => index < 10);
+        // this.data = data.filter((item, index) => index < 10);
         canvas.width = width * 2;
         canvas.height = height * 2;
         canvas.style.width = width + 'px';

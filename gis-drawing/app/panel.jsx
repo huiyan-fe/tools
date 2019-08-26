d2e039840b10bc1cdc96974fc80d1d6407232e7c
nikai <nikai@baidu.com>
2019-08-26 14:10:39 +0800

    更新九段线

--- gis-drawing/app/panel.jsx
+++ gis-drawing/app/panel.jsx
@@ -5,8 +5,8 @@ import DraggingLabel from './map/DraggingLabel.js';
 import center from './center.js';
 
 var pt = new BMap.Point(131.733142,  23.226515);
-var jiuduanwidth = 454;
-var jiuduanheight = 666;
+var jiuduanwidth = 452;
+var jiuduanheight = 672;
 var scale = 4.5;
 
 var jiuduanIcon = new BMap.Icon("./static/images/jiuduanxian.png", new BMap.Size(jiuduanwidth / scale, jiuduanheight / scale), {

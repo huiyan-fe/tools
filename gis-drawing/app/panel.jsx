cfffaade4fffef373289a800f81e3af6d6a87013
nikai <nikai@baidu.com>
2019-08-26 13:21:55 +0800

    更新九段线

--- gis-drawing/app/panel.jsx
+++ gis-drawing/app/panel.jsx
@@ -5,9 +5,9 @@ import DraggingLabel from './map/DraggingLabel.js';
 import center from './center.js';
 
 var pt = new BMap.Point(131.733142,  23.226515);
-var jiuduanwidth = 492;
-var jiuduanheight = 726;
-var scale = 5.0;
+var jiuduanwidth = 458;
+var jiuduanheight = 668;
+var scale = 4.5;
 
 var jiuduanIcon = new BMap.Icon("./static/images/jiuduanxian.png", new BMap.Size(jiuduanwidth / scale, jiuduanheight / scale), {
     imageSize: new BMap.Size(jiuduanwidth / scale, jiuduanheight / scale)
--- gis-drawing/build/bundle.js
+++ gis-drawing/build/bundle.js
@@ -22110,9 +22110,9 @@
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 	var pt = new BMap.Point(131.733142, 23.226515);
-	var jiuduanwidth = 492;
-	var jiuduanheight = 726;
-	var scale = 5.0;
+	var jiuduanwidth = 458;
+	var jiuduanheight = 668;
+	var scale = 4.5;
 
 	var jiuduanIcon = new BMap.Icon("./static/images/jiuduanxian.png", new BMap.Size(jiuduanwidth / scale, jiuduanheight / scale), {
 	    imageSize: new BMap.Size(jiuduanwidth / scale, jiuduanheight / scale)

965a6a66b67cbedadc0e584c3a21ce4bbe939769
liqi26 <liqi26@baidu.com>
2020-01-03 17:09:05 +0800

    发布github

---
+++ road3d-map/build/precache-manifest.9d9c752c060bef73b0171bba9f7f78ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf150c4444cb24bf8844be36a5df9671",
+    "url": "/index.html"
+  },
+  {
+    "revision": "401307df9885072f8cdb",
+    "url": "/static/css/2.245eb8db.chunk.css"
+  },
+  {
+    "revision": "178ddf0610804efd8cd1",
+    "url": "/static/css/main.40f41f4f.chunk.css"
+  },
+  {
+    "revision": "401307df9885072f8cdb",
+    "url": "/static/js/2.afa5d993.chunk.js"
+  },
+  {
+    "revision": "178ddf0610804efd8cd1",
+    "url": "/static/js/main.aea29390.chunk.js"
+  },
+  {
+    "revision": "9e01f6fedd9318fac31c",
+    "url": "/static/js/runtime-main.baffa2a0.js"
+  }
+]);

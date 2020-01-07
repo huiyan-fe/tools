a8524e33e01d13e310a7445e28b51135ec8507c9
liqi26 <liqi26@baidu.com>
2020-01-07 19:04:52 +0800

    比例问题暂时合入+4

--- road3d-map/src/App.js
+++ road3d-map/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import * as dat from 'dat.gui';
 import * as mapvgl from 'mapvgl';
+import Color from 'color';
 import h337 from 'heatmap.js';
 import TitleHeader from './components/TitleHeader';
 import Map3D from './components/Map3D';
@@ -9,23 +9,54 @@ import ShenhaiData from './data/chengdu_avgspeed_sort_work.json';
 
 import './App.less';
 
-function FizzyText(radius, max, min, color1, color2, color3, color4, color5) {
-    this.radius = radius || 4;
-    this.max = max || 10;
-    this.min = min || 0;
-    this.color1 = color1 || "#FF0000"; // CSS string
-    this.color2 = color2 || "#FFFF33"; // CSS string
-    this.color3 = color3 || "#66FF66"; // CSS string
-    this.color4 = color4 || "#00FF00"; // CSS string
-    this.color5 = color5 || "#0000FF"; // CSS string
+function FizzyText(radius, max, min, gradient, color1Value, color1, color2Value, color2, color3Value, color3, color4Value, color4) {
+    this.radius = radius;
+    this.max = max;
+    this.min = min;
+    this.gradient = gradient 
+
+    this.color1Value = color1Value;
+    this.color1 = color1;
+    this.color2Value = color2Value;
+    this.color2 = color2;
+    this.color3Value = color3Value;
+    this.color3 = color3;
+    this.color4Value = color4Value;
+    this.color4 = color4;
+
 }
+
+const gradientOptions = [
+    { 
+        0.25: "rgba(0, 0, 255, 1)",
+        0.55: "rgba(0, 255, 0, 1)",
+        0.85: "rgba(255, 255, 0, 1)",
+        1.0: "rgba(255, 0, 0, 1)"
+    },
+    { 
+        0.25: "rgba(255, 0, 0, 1)",
+        0.55: "rgba(255, 255, 0, 1)",
+        0.85: "rgba(0, 255, 0, 1)",
+        1.0: "rgba(0, 0, 255, 1)"
+    },
+    { 
+        0.25: "rgba(0, 0, 255, 1)",
+        0.55: "rgba(0, 255, 0, 1)",
+        0.85: "rgba(255, 255, 0, 1)",
+        1.0: 'rgba(255, 255, 255, 1)'
+    },
+    { 
+        0.25: 'rgba(255, 255, 255, 1)',
+        0.55: "rgba(255, 255, 0, 1)",
+        0.85: "rgba(0, 255, 0, 1)",
+        1: "rgba(0, 0, 255, 1)"
+    }
+  ];
 
 class App extends Component {
 
     canvasWidth = 298;
     canvasHeight = 96;
-    // canvasWidth = 298;
-    // canvasHeight = 96;
     state = {
         dataWeRender: ShenhaiData,
         innerHeight: window.innerHeight,
@@ -92,25 +123,27 @@ class App extends Component {
             map: map
         });
         // 初始化 GUI面板
-        this.initGUIPanel(4, heatMax, 0)
+        this.initGUIPanel(4, heatMax, 0, { 
+            0.25: "rgba(0, 0, 255, 1)",
+            0.55: "rgba(0, 255, 0, 1)",
+            0.85: "rgba(255, 255, 0, 1)",
+            1.0: "rgba(255, 0, 0, 1)"
+        },  heatMax * .25, "rgba(0, 0, 255, 1)",
+            heatMax * .55, "rgba(0, 255, 0, 1)",
+            heatMax * .85, "rgba(255, 255, 0, 1)",
+            heatMax * 1, "rgba(255, 0, 0, 1)")
         this.initCanvas();
         this.drawMapvgl();
         // this.drawTimeText();
     }
 
     initCanvas = () => {
-        const { color1, color2, color3, color4, color5 } = this.text
+        const { gradient } = this.text
 
         const nuConfig = {
             container: this.canvasContainer,
             blur: 1,
-            gradient: {
-                '.3': color1,
-                '.4': color2,
-                '.5': color3,
-                '.6': color4,
-                '1': color5,
-            },
+            gradient,
             backgroundColor: '#000'
         };
 
@@ -232,7 +265,6 @@ class App extends Component {
         const { lineData } = this.parseData(dataWeRender);
 
         if (this.layer) {
-            // this.drawTimeText();
             this.view.addLayer(this.layer);  
             this.layer.setData(lineData);
         }
@@ -265,27 +297,76 @@ class App extends Component {
                 texture: _this.mockCanvas,
             })
         }
-
-
         this.layerSetData()
         this.forceUpdate()
     }
 
-    changeHeatMapColor = () => {
-        const { dataWeRender } = this.state
-        const { color1, color2, color3, color4, color5 } = this.text
-        const { lineData } = this.parseData(dataWeRender);
+    getColorFromColorStops = (colorStops, percent) => { 
+        if (percent < 0) {
+          percent = 0;
+        }
+        if (percent > 1) {
+          percent = 1;
+        }
+        let i = 0;
+        for (let length = colorStops.length; i < length; ++i) {
+          const colorStop = colorStops[i];
+          if (colorStop[0] >= percent) {
+            break;
+          }
+        }
+        const startColorStop = colorStops[i - 1];
+        const endColorStop = colorStops[i];
+        let startPercent = 0;
+        let endPercent = 1;
+        let startColor = null;
+        let endColor = null;
+        if (startColorStop) {
+          startPercent = startColorStop[0];
+          startColor = startColorStop[1];
+        }
+        if (endColorStop) {
+          endPercent = endColorStop[0];
+          endColor = endColorStop[1];
+        }
+        if (!startColor) {
+          startColor = endColor || '#000';
+        }
+        if (!endColor) {
+          endColor = startColor || '#000';
+        }
+    
+        let relativePercent = (percent - startPercent) / (endPercent - startPercent);
+        const startColorObj = Color(startColor)
+        const endColorObj = Color(endColor);
+        const sr = startColorObj.red();
+        const sg = startColorObj.green();
+        const sb = startColorObj.blue();
+        const sa = startColorObj.alpha();
+        const er = endColorObj.red();
+        const eg = endColorObj.green();
+        const eb = endColorObj.blue();
+        const ea = endColorObj.alpha();
+        return `rgba(${(sr + (er - sr) * relativePercent).toFixed()},` +
+          `${(sg + (eg - sg) * relativePercent).toFixed()},` +
+          `${(sb + (eb - sb) * relativePercent).toFixed()},` +
+          `${(sa + (ea - sa) * relativePercent).toFixed()})`;
+      }
+
+    changeHeatMapAllColor = () => {
+        const { max, color1Value, color2Value, color3Value, color4Value, gradient} = this.text
+
         const nuConfig = {
-            gradient: {
-                '.3': color1,
-                '.4': color2,
-                '.5': color3,
-                '.6': color4,
-                '1': color5,
-            },
+            gradient,
             backgroundColor: '#000'
         };
+
         
+        this.color1.setValue(nuConfig.gradient[color1Value / max])
+        this.color2.setValue(nuConfig.gradient[color2Value / max])
+        this.color3.setValue(nuConfig.gradient[color3Value / max])
+        this.color4.setValue(nuConfig.gradient[color4Value / max])        
+
         this.heatmap.configure(nuConfig)
 
         this.mockCtx = this.mockCanvas.getContext('2d');
@@ -301,7 +382,50 @@ class App extends Component {
                 texture: _this.mockCanvas,
             })
         }
+        this.layerSetData()
+        this.forceUpdate()
+    }
+    changeHeatMapColor = () => {
+        const { max, color1Value, color2Value, color3Value, color4Value, gradient, color1, color2, color3, color4} = this.text
+        
+        const colorArr = [
+            [0.25, color1],
+            [0.55, color2],
+            [0.85, color3],
+            [1, color4]
+        ];
 
+        const nuConfig = {
+            gradient: {
+                [color1Value / max] : this.getColorFromColorStops(colorArr, color1Value / max),
+                [color2Value / max] : this.getColorFromColorStops(colorArr, color2Value / max),
+                [color3Value / max] : this.getColorFromColorStops(colorArr, color3Value / max),
+                [color4Value / max] : this.getColorFromColorStops(colorArr, color4Value / max),
+            },
+            backgroundColor: '#000'
+        };      
+
+        this.color1.setValue(nuConfig.gradient[color1Value / max])
+        this.color2.setValue(nuConfig.gradient[color2Value / max])
+        this.color3.setValue(nuConfig.gradient[color3Value / max])
+        this.color4.setValue(nuConfig.gradient[color4Value / max])        
+
+
+        this.heatmap.configure(nuConfig)
+
+        this.mockCtx = this.mockCanvas.getContext('2d');
+        this.mockCtx.clearRect(0, 0, this.mockCanvas.width, this.mockCanvas.height)
+        let imageData = this.heatmap._renderer.canvas.toDataURL();
+
+        this.img.src = imageData
+    
+        const _this = this
+        this.img.onload = function () {
+            _this.mockCtx.drawImage(_this.img, 0, 0, 299, 96, 0, 0, 256, 128);
+            _this.layer.setOptions({
+                texture: _this.mockCanvas,
+            })
+        }
         this.layerSetData()
         this.forceUpdate()
     }
@@ -312,35 +436,60 @@ class App extends Component {
         this.setState({ visible: !visible })
     }
       
-    initGUIPanel = (radius, max, min, color1, color2, color3, color4, color5) => {
+    initGUIPanel = (radius, max, min, gradient, color1Value, color1, color2Value, color2, color3Value, color3, color4Value, color4) => {
      
-        this.gui = new dat.GUI();
+        this.gui = new window.dat.GUI({
+            nameMap: {
+                radius: '辐射半径',
+                min: '最小阈值',
+                max: '最大阈值',
+                gradient: '渐变色',
+                color1Value: '一档位值',
+                color1: '一档颜色',
+                color2Value: '二档位值',
+                color2: '二档颜色',
+                color3Value: '三档位值',
+                color3: '三档颜色',
+                color4Value: '四档位值',
+                color4: '四档颜色',
+            }
+        });
 
-        this.text = new FizzyText(radius, max, min, color1, color2, color3, color4, color5);
+        this.text = new FizzyText(radius, max, min, gradient, color1Value, color1, color2Value, color2, color3Value, color3, color4Value, color4);
    
         this.setState({ text: this.text })
         
-        this.radiusText = this.gui.add(this.text, 'radius');
-
+        this.radiusText = this.gui.add(this.text, 'radius').min(0.1);
         this.minText = this.gui.add(this.text, 'min');
         this.maxText = this.gui.add(this.text, 'max');
 
-        this.color1 = this.gui.addColor(this.text, 'color1');
-        this.color2 = this.gui.addColor(this.text, 'color2');
-        this.color3 = this.gui.addColor(this.text, 'color3');
-        this.color4 = this.gui.addColor(this.text, 'color4');
-        this.color5 = this.gui.addColor(this.text, 'color5');
-
         this.radiusText.onFinishChange(this.changeHeatMap);
-
         this.minText.onFinishChange(this.changeHeatMap);
         this.maxText.onFinishChange(this.changeHeatMap);
+        
+        this.gradientOptions = this.gui.addGradient(this.text, 'gradient', gradientOptions);
+        this.gradientOptions.onFinishChange(this.changeHeatMapAllColor);
 
-        this.color1.onFinishChange(this.changeHeatMapColor)
-        this.color2.onFinishChange(this.changeHeatMapColor)
-        this.color3.onFinishChange(this.changeHeatMapColor)
-        this.color4.onFinishChange(this.changeHeatMapColor)
-        this.color5.onFinishChange(this.changeHeatMapColor)
+        // 第一档
+        this.color1Value = this.gui.add(this.text, 'color1Value');
+        this.color1Value.onFinishChange(this.changeHeatMapColor);
+        this.color1 = this.gui.addColor(this.text, 'color1');
+        this.color1.onFinishChange(this.changeHeatMapColor);
+        // 第二档
+        this.color2Value = this.gui.add(this.text, 'color2Value');
+        this.color2Value.onFinishChange(this.changeHeatMapColor);
+        this.color2 = this.gui.addColor(this.text, 'color2');
+        this.color2.onFinishChange(this.changeHeatMapColor);
+        // 第三档
+        this.color3Value = this.gui.add(this.text, 'color3Value');
+        this.color3Value.onFinishChange(this.changeHeatMapColor);
+        this.color3 = this.gui.addColor(this.text, 'color3');
+        this.color3.onFinishChange(this.changeHeatMapColor);
+        // 第四档
+        this.color4Value = this.gui.add(this.text, 'color4Value');
+        this.color4Value.onFinishChange(this.changeHeatMapColor);
+        this.color4 = this.gui.addColor(this.text, 'color4');
+        this.color4.onFinishChange(this.changeHeatMapColor);
 
         this.gui.__controllers.forEach(e => {
             e.updateDisplay()
@@ -382,6 +531,7 @@ class App extends Component {
                     <p>当前值：{(selectValue.showPoint[0][2]).toFixed(2)}</p>
                 </div>}
                 <div ref={this.tRef} className="dashboard">请将数据拖入</div>
+                {/* <input type='file' ref={this.tRef} className="fileUpload" onChange={this.onDropUploadClick}></input> */}
                 <div className="change" onClick={this.onChangeClick}><b>切换2D/3D</b></div>
                 <Map3D
                     style={{ height: innerHeight }}
@@ -389,7 +539,6 @@ class App extends Component {
                     center={[11586045.04, 3566065.08]}
                     zoom={11}
                     onMapLoaded={this.onMapLoaded}
-                    // changeHeatMap={this.changeHeatMap}
                 >
                 </Map3D>
                 {text &&
@@ -399,7 +548,6 @@ class App extends Component {
                         dataWeRender={dataWeRender}
                         text={text}
                         visible={visible}
-                        // changeHeatMap={this.changeHeatMap}
                         selectValue={selectValue}
                     >
                     </Map2D> 
